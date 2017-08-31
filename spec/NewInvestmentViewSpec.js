1d0587626930b5a3d6b3b462af8d166b911a05e0
Kevin Cartwright <kevincart@gmail.com>
2017-08-31 16:35:11 +1200

    working on the new investment view, chapter 3

--- spec/NewInvestmentViewSpec.js
+++ spec/NewInvestmentViewSpec.js
@@ -1,6 +1,12 @@
 describe("NewInvestmentView", function() {
+  var view;
   beforeEach(function() {
     loadFixtures('NewInvestmentView.html');
+    view = new NewInvestmentView({selector: '#new-investment'});
+  });
+  
+  it("should expose a property with its DOM element", function() {
+    expect(view.$el).toExist();
   });
   
   it("should allow the input of the stock symbol", function() {
--- src/NewInvestmentView.js
+++ src/NewInvestmentView.js
@@ -2,7 +2,7 @@
 // Advantage of IIFE is it helps avoid use of global declarations, everything is local 
 (function ($, Investment, Stock) {
   function NewInvestmentView (params) {
-    
+    this.$el = $(params.selector);
   };
   
   this.NewInvestmentView = NewInvestmentView;
