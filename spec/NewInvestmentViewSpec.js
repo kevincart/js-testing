91771dffb5a55db7dcf65b228fa0bfc40b0b41ef
Kevin Cartwright <kevincart@gmail.com>
2017-08-31 15:54:15 +1200

    started NewInvestmentSpec.js from chapter 3

---
+++ spec/NewInvestmentViewSpec.js
@@ -0,0 +1,13 @@
+describe("NewInvestmentView", function() {
+  it("should allow the input of the stock symbol", function() {
+    
+  });
+  
+  it("should allow the input of shares", function() {
+    
+  });
+  
+  it("should allow the input of the share price", function() {
+    
+  });
+});
---
+++ src/NewInvestmentView.js
@@ -0,0 +1,9 @@
+// Initialize new Investment view module with IIFE (Immediately Invoked Function Expression)
+// Advantage of IIFE is it helps avoid use of global declarations, everything is local 
+(function ($, Investment, Stock) {
+  function NewInvestmentView (params) {
+    
+  };
+  
+  this.NewInvestmentView = NewInvestmentView;
+}) (jQuery, Investment, Stock);
